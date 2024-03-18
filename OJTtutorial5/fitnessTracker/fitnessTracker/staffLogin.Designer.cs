6f6b769b55e13ed32d1116d0c8a1754aaf113342
nearlyshippe <136670434+nearlyshippe@users.noreply.github.com>
2024-03-18 08:21:48 +0630

    Add files via upload

---
+++ OJTtutorial5/fitnessTracker/fitnessTracker/staffLogin.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace fitnessTracker
+{
+    partial class staffLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStaffNo = new System.Windows.Forms.Label();
+            this.txtStaffNo = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.accountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblStaffNo
+            // 
+            this.lblStaffNo.AutoSize = true;
+            this.lblStaffNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStaffNo.Location = new System.Drawing.Point(175, 140);
+            this.lblStaffNo.Name = "lblStaffNo";
+            this.lblStaffNo.Size = new System.Drawing.Size(106, 29);
+            this.lblStaffNo.TabIndex = 0;
+            this.lblStaffNo.Text = "Staff No";
+            this.lblStaffNo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtStaffNo
+            // 
+            this.txtStaffNo.Location = new System.Drawing.Point(346, 147);
+            this.txtStaffNo.Name = "txtStaffNo";
+            this.txtStaffNo.Size = new System.Drawing.Size(121, 22);
+            this.txtStaffNo.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(392, 333);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 32);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(180, 333);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 32);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Location = new System.Drawing.Point(175, 210);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(128, 29);
+            this.lblPass.TabIndex = 4;
+            this.lblPass.Text = "Password";
+            this.lblPass.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(346, 210);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(121, 22);
+            this.txtPwd.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.accountToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(706, 28);
+            this.menuStrip1.TabIndex = 50;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // accountToolStripMenuItem
+            // 
+            this.accountToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loginToolStripMenuItem,
+            this.registerToolStripMenuItem});
+            this.accountToolStripMenuItem.Name = "accountToolStripMenuItem";
+            this.accountToolStripMenuItem.Size = new System.Drawing.Size(77, 26);
+            this.accountToolStripMenuItem.Text = "Account";
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(146, 26);
+            this.loginToolStripMenuItem.Text = "Login";
+            // 
+            // registerToolStripMenuItem
+            // 
+            this.registerToolStripMenuItem.Name = "registerToolStripMenuItem";
+            this.registerToolStripMenuItem.Size = new System.Drawing.Size(146, 26);
+            this.registerToolStripMenuItem.Text = "Register";
+            this.registerToolStripMenuItem.Click += new System.EventHandler(this.registerToolStripMenuItem_Click);
+            // 
+            // staffLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 502);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtStaffNo);
+            this.Controls.Add(this.lblStaffNo);
+            this.Name = "staffLogin";
+            this.Text = "staffLogin";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStaffNo;
+        private System.Windows.Forms.TextBox txtStaffNo;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem accountToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registerToolStripMenuItem;
+    }
+}
---
+++ OJTtutorial5/fitnessTracker/fitnessTracker/staffLogin.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fitnessTracker
+{
+    public partial class staffLogin : Form
+    {
+        String key = "b14ca5898a4e4133bbce2ea2315a1916";
+        staffDataTableAdapters.staffInfoTableAdapter staffTA=new staffDataTableAdapters.staffInfoTableAdapter();
+        DataTable staffData=new DataTable();
+        int loginCount = 0;
+        public staffLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void registerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txt staffregister = new txt();
+            this.Hide();
+            staffregister.ShowDialog();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (loginCount == 3)
+                {
+                    MessageBox.Show("Fail Login! Try Again Later!");
+                }
+                else
+                {
+                    if (String.IsNullOrWhiteSpace(txtStaffNo.Text))
+                    {
+                        MessageBox.Show("Enter Staff No :");
+                    }
+                    else if (String.IsNullOrWhiteSpace(txtPwd.Text))
+                    {
+                        MessageBox.Show("Enter Valid Password:");
+                    }
+                    else
+                    {
+                        StaffClass staff = new StaffClass();
+                        staff.StaffNO = txtStaffNo.Text;
+                        staff.EncryptedPwd = aesOp.EncryptString(key, txtPwd.Text);
+                        staffData = staffTA.staffLogin(staff.StaffNO, staff.EncryptedPwd);
+                        if (staffData.Rows.Count>0)
+                        {
+                            MessageBox.Show("Login Successful!");
+                        }
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            
+            
+        }
+    }
+}
